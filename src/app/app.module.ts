ad614b9b12afaff792ca292c25d2a19a16b1f5e8
Aleksander717 <102306884+Aleksander717@users.noreply.github.com>
2022-04-25 17:49:15 +0400

    ASL-03: Removing blank lines

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -19,9 +16,3 @@ import { MatSliderModule } from '@angular/material/slider';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
-
-
-
-
